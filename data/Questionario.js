97afc2949b54937bdcb3f416a00fcc72b8e99cc7
moralesvictorr <vmmorales@unicauca.edu.co>
2023-01-31 18:05:40 -0500

    feat: folders & files added

---
+++ data/Questionario.js
@@ -0,0 +1,255 @@
+export const questionHTML = [
+    {
+        
+        question: "¿Qué significa el acrónimo HTML?",
+        a: "Hyper Text Mask Language",
+        b: "Hard Text Markup Language",
+        c: "Hyper Text Markup Language",
+        d: "Hard Text Mask Language",
+        corrrect: "Hyper Text Markup Language"
+    },
+    {
+        
+        question: "¿Qué etiqueta es correcta para indicar un salto de línea",
+        a: "</br>",
+        b: "<br/>",
+        c: "<breakLine>",
+        d: "<hr>",
+        corrrect: "</br>"
+    },
+    {
+        
+        question: "¿Cuál es la forma correcta de escribir un comentario en HTML?",
+        a: "/* */",
+        b: "<!–…–>",
+        c: "ctrl + c",
+        d: "<¡–…–>r",
+        corrrect: "<!–…–>"
+    },
+    {
+        
+        question: "¿Cuál es la forma correcta de escribir un enlace en HTML?",
+        a: "<a title=» «>…</>",
+        b: "<a href=» «>…</a>",
+        c: "<a src=» «>…</a>",
+        d: "<link href=» «>…<link>",
+        corrrect: "<a href=» «>…</a>"
+    },
+    {
+        
+        question: "¿Qué etiqueta se usa para escribir una lista no ordenada?",
+        a: "<ol>",
+        b: "<ul>",
+        c: "<li>",
+        d: "<list>",
+        corrrect: "<ul>"
+    },
+    {
+        
+        question: "¿Qué etiqueta de HTML nos sirve para incluir archivos de JavaScript?",
+        a: "<style>",
+        b: "<script>",
+        c: "<main>",
+        d: "<header>",
+        corrrect: "<script>"
+    },
+]
+
+export const questionCSS = [
+    {
+        
+        question: "¿Qué significa CSS?",
+        a: "Color Style Sheets",
+        b: "Code of Style Sheets",
+        c: "Coded Style Sheets ",
+        d: "Cascading Style Sheets",
+        corrrect: "Cascading Style Sheets"
+    },
+    {
+        
+        question: "¿Cómo podemos añadir un comentario en una hoja de estilo CSS?",
+        a: "<!-- -->",
+        b: "/**/",
+        c: "!!",
+        d: "//node_modules",
+        corrrect: "/**/"
+    },
+    {
+        
+        question: "¿Cómo se cambia el valor del margen derecho de un elemento?",
+        a: "margin-right",
+        b: "right-margin",
+        c: "marginright",
+        d: "Ninguna de las anteriores",
+        corrrect: "margin-right"
+    },
+    {
+        
+        question: "¿Cuál es la forma más eficiente de aplicar estilos CSS en un documento HTML?",
+        a: "En línea",
+        b: "Incrustado en la cabecera",
+        c: "Hojas de estilo externas",
+        d: "Mediante PHP",
+        corrrect: "Hojas de estilo externas"
+    },
+    {
+        
+        question: "¿Cuáles de las siguientes declaraciones son propiedades o atributos?",
+        a: "span",
+        b: "italic",
+        c: "background-color",
+        d: "h1",
+        corrrect: "background-color"
+    },
+
+    {
+        
+        question: "¿Cómo se define un color de fondo para todas las etiquetas <p> en CSS?",
+        a: "p.all {background-color:red;}",
+        b: "p {background-color:red;}",
+        c: "#p {background-color:red;}",
+        d: "r.p {background-color:red;}",
+        corrrect: "p {background-color:red;}"
+    }
+   
+]
+
+export const questionJS = [
+    {
+        
+        question: "¿Como debo exportar una Clase?",
+        a: "export default { ClassName }",
+        b: "export { ClassName }",
+        c: "export default ClassName",
+        d: "export  ClassName ",
+        corrrect: "export default ClassName"
+    },
+    {
+        
+        question: "¿Como se reepresenta al Spread Syntax?",
+        a: "&&",
+        b: "...",
+        c: "||",
+        d: "//",
+        corrrect: "..."
+    },
+    {
+        
+        question: "JavaScript fue diseñado por _____________ en 1995.",
+        a: "Microsoft",
+        b: "Apple",
+        c: "Netscape",
+        d: "Novel",
+        corrrect: "Netscape"
+    },
+    {
+        
+        question: " Tipo de dato JavaScript para manejar números'",
+        a: " int",
+        b: "integer",
+        c: "long",
+        d: "number",
+        corrrect: "number"
+    },
+    {
+        
+        question: "¿undefined es un tipo de datos JavaScript?",
+        a: "Verdadero",
+        b: "Falso",
+        c: "a veces",
+        d: "masomenos",
+        corrrect: "Verdadero"
+    },
+    
+ 
+]
+export const questionFigma = [
+    {
+                
+        question: "¿Qué es Figma?",
+        a: "Es una pagina que permite programar ",
+        b: "Es una herramienta online para diseñar interfaces que permite la colaboración en vivo.",
+        c: "Es una herramienta de diseño",
+        d: "Todas las anteriores",
+        corrrect: "Es una herramienta online para diseñar interfaces que permite la colaboración en vivo."
+    },
+    {
+        
+        question: "¿En que año fue su aparicion?",
+        a: "2015",
+        b: "2005",
+        c: "1990",
+        d: "2000",
+        corrrect: "2015"
+    },
+    {
+        
+        question: "¿Que puedo crear en Figma ",
+        a: "App",
+        b: "Pantallas de sitios web, Aplicaciones para celulares.",
+        c: "Base de datos",
+        d: "Sitios web",
+        corrrect: "Pantallas de sitios web, Aplicaciones para celulares."
+    },
+    {
+        
+        question: " ¿Figma ofrece almacenamiento ilimitado?",
+        a: " Falso",
+        b: "Verdadero",
+        c: "segun la region",
+        d: "en ocaciones",
+    
+        corrrect: "Verdadero"
+    },
+    {
+        
+        question: " ¿cual de estas NO es una característica de Figma?",
+        a: "Lo más interesante de Figma es su usabilidad. Es intuitiva y fácil de utilizar.",
+        b: "Puedes exportar en formatos estándar como .pdf, .png, .jpg.",
+        c: "puedes desarrollar un sitio web",
+        d: "Te permite sistematizar todos tus diseños y alojarlos en la nube",
+    
+        corrrect: "puedes desarrollar un sitio web"
+    }
+ 
+]
+
+export const questionUX = [
+    {
+        question: "¿Qué significa UX?",
+        a: "User Expert",
+        b: "Use Experience",
+        c: "User Experience",
+        d: "User Excommunicate",
+        corrrect: "User Experience"
+    },{
+        question: "¿Qué es la Experiencia del usuario?",
+        a: "los trabajos que ha tenido el usuario",
+        b: "tiempos dificiles en la empresa",
+        c: "desarrollo de un producto y su relacion con quien lo creó",
+        d: "consiste en la experiencia generada por las interacciones de un usuario con determinada solución o producto de una marca.",
+        corrrect: "consiste en la experiencia generada por las interacciones de un usuario con determinada solución o producto de una marca."
+    },{
+        question: "¿Cual de éstas es una de las áreas de UX?",
+        a: "UX developer",
+        b: "UX sports",
+        c: "UX trasnport",
+        d: "User Excommunicate",
+        corrrect: "UX developer"
+    },{
+        question: "¿Qué es UX Writing?",
+        a: "es una estrategia enfocada en el diseño de una app",
+        b: "es una estrategia enfocada en el contenido (textos) de las soluciones que son entregadas a los usuarios. ",
+        c: "User Experience",
+        d: "es una estrategia enfocada en el desarrollo de juegos",
+        corrrect: "es una estrategia enfocada en el contenido (textos) de las soluciones que son entregadas a los usuarios. "
+    },   {
+        question: "¿Cuál es la diferencia entre UI y UX?",
+        a: "Ambos, UI y UX, están relacionados y forman parte de la estrategia de diseño de un producto.",
+        b: "UI es la abreviatura de User Interface, que se traduce como interfaz del usuario",
+        c: "La principal diferencia entre ellos es que UI se enfoca en el layout o diseño en sí, mientras que UX se centra en la experiencia del usuario",
+        d: "User Excommunicate",
+        corrrect: "La principal diferencia entre ellos es que UI se enfoca en el layout o diseño en sí, mientras que UX se centra en la experiencia del usuario al interactuar con la solución como un todo."
+    }
+ 
+]
--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
-import Form from './Containers/Form';
-import BannerPromo from './Components/BannerPromo';
-import Description from './Components/Description';
-import "./Styles/app.scss"
-import "./Styles/bannerPromo.scss"
-import "./Styles/descrption.scss"
-import "./Styles/form.scss"
-
 
 function App() {
   return (
     <>
-      <div className='principal-container'>
-        <div className='description-container'>
-        <Description />
-        </div>
-
-        <div className='section-container'>
-          <BannerPromo />
-          <Form />
-        </div>
-
-      </div>
+    <h1>Hola</h1>
     </>
 
   );
