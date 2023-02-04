3c1d454ea4a1c15d5393c79d7b4068c684f93a36
Marianscabas <mariancabanacabas98@gmail.com>
2023-02-03 22:08:34 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/moralesvictorr/workshop4 into Marian""
    This reverts commit 3d81093a2c5771beae5cecf58d1847d5a849b90e.

--- src/components/CompLogin.jsx
+++ src/components/CompLogin.jsx
@@ -3,6 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const CompLogin = () => {
     const {loginWithRedirect} = useAuth0();
+   // document.addEventListener('DOMContentLoaded', loginWithRedirect() )
     return <button onClick={() => loginWithRedirect()}>Log In</button>;
 }
 
