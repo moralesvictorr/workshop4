9d78abfa40cec353d50d0bf64391bbf8699174db
JulioJavier <01juliomtz@gmail.com>
2023-02-04 21:01:49 -0500

    feat: correccion de styles del nabvar y home. se crean styles de la pagina Statistics

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Timer from "../components/Timer";
 
 const Navbar = () => {
   return (
-    <div className=" bg-slate-800 ">
-      <div className=" bg-cyan-900 flex justify-around ">
+    <div className=" bg-slate-800  fixed bottom-0 w-screen text-center">
+      <div className=" bg-cyan-900 flex justify-around items-center p-1">
        
-        <Link to="/home" >
-          <img src="./icono-home.svg" width="40px" alt="icono-home" />
+        <Link to="/home" className="flex flex-col justify-center items-center">
+          <img className="" src="./icono-home.svg" width="40px" alt="icono-home" />
           <span className="text-white text-sm font-anton hover:text-green-500">
             HOME
           </span>
         </Link>
 
-
-        <Link to="/statistics" >
+        <Link to="/statistics" className="flex flex-col justify-center items-center" >
           <img
             src="./icono-statistics.svg"
             width="40px"
@@ -27,7 +25,7 @@ const Navbar = () => {
         </Link>
 
 
-        <Link to="/profile">
+        <Link to="/profile" className="flex flex-col justify-center items-center">
           <img src="./icono-profile.svg" width="40px" alt="icono-profile" />
           <span className="text-white text-sm font-anton hover:text-green-500">
             PROFILE
