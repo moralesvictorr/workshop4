b52e97d8d97a1cfb8501ae4335d029165baffe95
Marianscabas <mariancabanacabas98@gmail.com>
2023-02-08 18:42:17 -0500

    prueba

--- src/components/CompLogin.jsx
+++ src/components/CompLogin.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
 const CompLogin = () => {
-  const {loginWithRedirect} = useAuth0();
+  const {loginWithRedirect} = useAuth0(); 
+     console.log("hola")
   return (
-    
+
       <button
       className='text-slate-800'
       onClick={() =>{loginWithRedirect()}}>Login</button>
