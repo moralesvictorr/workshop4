f5f450c7f731e3efff41be35bcaeaece60c23acd
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-08 18:31:17 -0500

    feat: Prueba

--- src/pages/Questionary.jsx
+++ src/pages/Questionary.jsx
@@ -29,7 +29,7 @@ const getQuestionsArray = (category) => {
   return questionData[category] // Vector con las preguntas de la categoria seleccionada
 }
 
-
+ console.log("Prueba")
 
 /* EMPIEZA EL COMOPONENTE QUESTIONARY */
 const Questionary = () => {
