ccdd1b5080d60aff4e70b91e8583868f86b2ed24
JulioJavier <01juliomtz@gmail.com>
2023-02-09 00:06:14 -0500

    feat: reverse changes

--- src/Router.js
+++ src/Router.js
@@ -5,19 +5,13 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Statistics from "./pages/Statistics";
 import Questionary from "./pages/Questionary";
-// add
-import { useAuth0 } from "@auth0/auth0-react";
 
-const {loginWithRedirect} = useAuth0(); 
-console.log("Login Router")
 export const router = createBrowserRouter(
-	createRoutesFromElements(
-		<>
-                {/* add         */}
-				<Route path="/" component={loginWithRedirect()} />
+    createRoutesFromElements(
+        <>
 
+                {/* <Route path="/login" element={<Login />} /> */}
 				<Route path="/home" element={<Home />} />
-                <Route path="/login" element={<Login />} />
                 <Route path="/statistics" element={<Statistics />} />
 				<Route path="/profile" element={<Profile />} />
                 <Route path="/HTML" element={<Questionary />} />
@@ -25,7 +19,7 @@ export const router = createBrowserRouter(
                 <Route path="/JS" element={<Questionary />} />
                 <Route path="/FIGMA" element={<Questionary />} />
                 <Route path="/UX" element={<Questionary />} />
-                {/* <Route path="*" element={<Home />} /> */}
+                <Route path="*" element={<Home />} />
 
 		</>
 	)
