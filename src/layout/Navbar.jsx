cd03e48b16872bff485216a5f3f45629e20abf1a
JulioJavier <01juliomtz@gmail.com>
2023-02-09 12:26:08 -0500

    feat: correction hover icon Navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,36 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faChartSimple } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+// import { faUser } from '@fortawesome/free-regular-svg-icons';
 const Navbar = () => {
   return (
     <div className=" bg-slate-800  fixed bottom-0 w-screen text-center">
       <div className=" bg-cyan-900 flex justify-around items-center p-1">
-       
-        <Link to="/home" className="flex flex-col justify-center items-center">
-          <img className="" src="./icono-home.svg" width="40px" alt="icono-home" />
-          <span className="text-white text-sm font-anton hover:text-green-500">
-            HOME
-          </span>
+        <Link to="/home" className="flex flex-col justify-center items-center text-sm font-anton text-white hover:text-green-500 ">
+          <FontAwesomeIcon  icon={faHome} size="2x" color="inherit" />
+            H O M E
         </Link>
 
-        <Link to="/statistics" className="flex flex-col justify-center items-center" >
-          <img
-            src="./icono-statistics.svg"
-            width="40px"
-            alt="icono-statistics"
-          />
-          <span className="text-white text-sm font-anton hover:text-green-500">
-            STATISTICS
-          </span>
+        <Link to="/statistics" className="
+        flex
+        flex-col 
+        justify-center 
+        items-center
+        text-white 
+        text-sm 
+        font-anton 
+        hover:text-green-500
+        " >
+          <FontAwesomeIcon  icon={faChartSimple} size="2x" color="inherit" />
+          S T A T I S T I C S
         </Link>
 
 
-        <Link to="/profile" className="flex flex-col justify-center items-center">
-          <img src="./icono-profile.svg" width="40px" alt="icono-profile" />
-          <span className="text-white text-sm font-anton hover:text-green-500">
-            PROFILE
-          </span>
+        <Link to="/profile" className="
+        flex 
+        flex-col 
+        justify-center 
+        items-center 
+        text-white 
+        text-sm 
+        font-anton 
+        hover:text-green-500">
+        <FontAwesomeIcon  icon={faUser} size="2x"/>
+            P E R F I L
         </Link>
+
+
+
       </div>
     </div>
   );
